0a14c4af044755246a0c737a12cbdfa3c9604ca4
nohandsome <1797695463@qq.com>
2022-05-13 15:48:42 +0800

    create T_bot

---
+++ bot/data/templatematch_bot/T_bot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 吴骏 on 2022/5/13.
+//
+
+#include "T_bot.h"
---
+++ bot/data/templatematch_bot/T_bot.h
@@ -0,0 +1,14 @@
+//
+// Created by 吴骏 on 2022/5/13.
+//
+
+#ifndef HW2022_CHATBOT_T_BOT_H
+#define HW2022_CHATBOT_T_BOT_H
+
+
+class T_bot {
+
+};
+
+
+#endif //HW2022_CHATBOT_T_BOT_H
