2e9bad2b24201c34e692d4a183d9871458231918
nohandsome <1797695463@qq.com>
2022-05-13 23:15:17 +0800

    add T_bot fram

--- bot/data/templatematch_bot/T_bot.cpp
+++ bot/data/templatematch_bot/T_bot.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "T_bot.h"
+
+void T_bot::read_qa_into_map(std::map <std::string, std::vector<std::string>> &qa_map) {
+    //TODO:本函数用来将./q&a/qa.txt中的question作为key，answer作为对应的value读入map中
+    //记得裁剪掉q:,a:
+
+}
+
+void T_bot::calculate_score(const std::string input_s, std::vector <std::string> &highest_score_q,
+                            const std::map <std::string, std::vector<std::string>> qa_map) {
+    //本函数用于计算map中每个key与input_s的匹配得分
+    //TODO:将匹配得分最高的那几个key对应的string放入highest_score_q中即可
+
+
+}
+
+
+std::string T_bot::speak(const std::string input_s) {
+    //该函数综合运用上述几个函数
+    //实现用户输入一个string时，能够对应返回一个string即可
+    //使用完记得清除highest_score_q用于下次计算
+    //init
+    std::string output_s;
+    std::map <std::string, std::vector<std::string>> qa_map = T_bot::get_map();
+    std::vector <std::string> highest_score_q = T_bot::get_highest_score_q();
+    T_bot::read_qa_into_map(qa_map);
+    T_bot::calculate_score(input_s, highest_score_q, qa_map);
+    //TODO:利用随机数来选择一个string作为map的key，再利用随机数从map对应的value中得到一个答案作为返回值即可
+
+
+    //TODO:清除highest_score_q中的元素来用于下一次计算
+
+
+
+    //return
+    return output_s;
+}
--- bot/data/templatematch_bot/T_bot.h
+++ bot/data/templatematch_bot/T_bot.h
@@ -4,10 +4,33 @@
 
 #ifndef HW2022_CHATBOT_T_BOT_H
 #define HW2022_CHATBOT_T_BOT_H
+#include <map>
+#include <vector>
+#include <string>
 
 
 class T_bot {
 
+private:
+    //qa_map
+    //一个用于存放问题和回答的对应关系的map
+    //即每个问题的string对应了多个回答的string
+    //最后得到的answer是会利用随机数送这多个回答中取出一个即可
+    std::map<std::string, std::vector<std::string>> qa_map;
+    //用来存放得分最大的问题string
+    std::vector<std::string> highest_score_q;
+public:
+    //用来得到qa_map这一私有变量
+    std::map<std::string, std::vector<std::string>> get_map(){return this->qa_map;}
+    //用来得到highest_score_q这一私有变量
+    std::vector<std::string> get_highest_score_q(){return this->highest_score_q;}
+    //用来将./q&a/qa.txt中的qa读入map
+    void read_qa_into_map(std::map<std::string, std::vector<std::string>>& qa_map);
+    //用来计算匹配，将最大的几个匹配的string放入highest_score_q中
+    //!!!记住每次应当清空highest_score_q后再使用
+    void calculate_score(const std::string input_s, std::vector<std::string>& highest_score_q, const std::map<std::string, std::vector<std::string>> qa_map);
+    //利用随机数将highest_score_q中的某个string取出，作为map的key求出对应的value作为返回值
+    std::string speak(const std::string input_s);
 };
 
 
